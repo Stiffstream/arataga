40196a931565c21974e8cc0408b3b77df10db3f1
Yauheni Akhotnikau <eao197@gmail.com>
2021-05-20 14:22:32 +0300

    Extended version of mass_load.

--- tests/mass_load/main.cpp
+++ tests/mass_load/main.cpp
@@ -14,6 +14,20 @@
 namespace mass_load
 {
 
+[[nodiscard]]
+std::chrono::milliseconds
+no_delay_marker()
+{
+	return std::chrono::milliseconds::zero();
+}
+
+[[nodiscard]]
+bool
+is_delay_defined( std::chrono::milliseconds max_value )
+{
+	return max_value != std::chrono::milliseconds::zero();
+}
+
 struct cmd_line_args_t
 {
 	std::uint16_t m_port_range_left{ 3000u };
@@ -25,6 +39,10 @@ struct cmd_line_args_t
 	asio::ip::address_v4 m_proxy_addr;
 	asio::ip::address_v4 m_target_addr;
 	std::uint16_t m_target_port{ 8080u };
+
+	std::chrono::milliseconds m_max_connect_delay{ no_delay_marker() };
+	std::chrono::milliseconds m_max_send_delay{ no_delay_marker() };
+	std::chrono::milliseconds m_max_receive_delay{ no_delay_marker() };
 };
 
 [[nodiscard]]
@@ -88,6 +106,21 @@ parse_cmd_line( int argc, char ** argv )
 			fmt::format( "Set the target port (default: {})",
 					result.m_target_port ),
 			{ "target-port" } );
+
+	args::ValueFlag< std::uint16_t > max_connect_delay( parser,
+			"max-connect-delay",
+			"Set max delay for random pause before connect. Milliseconds",
+			{ "max-connect-delay" } );
+	args::ValueFlag< std::uint16_t > max_send_delay( parser,
+			"max-send-delay",
+			"Set max delay for random pause before sending the request. "
+					"Milliseconds",
+			{ "max-send-delay" } );
+	args::ValueFlag< std::uint16_t > max_receive_delay( parser,
+			"max-receive-delay",
+			"Set max delay for random pause before receiving a response. "
+					"Milliseconds",
+			{ "max-receive-delay" } );
 
 	try
 	{
@@ -179,6 +212,25 @@ parse_cmd_line( int argc, char ** argv )
 		return std::nullopt;
 	}
 
+	if( max_connect_delay )
+	{
+		result.m_max_connect_delay = std::chrono::milliseconds{
+				static_cast<std::int32_t>( args::get( max_connect_delay ) )
+		};
+	}
+	if( max_send_delay )
+	{
+		result.m_max_send_delay = std::chrono::milliseconds{
+				static_cast<std::int32_t>( args::get( max_send_delay ) )
+		};
+	}
+	if( max_receive_delay )
+	{
+		result.m_max_receive_delay = std::chrono::milliseconds{
+				static_cast<std::int32_t>( args::get( max_receive_delay ) )
+		};
+	}
+
 	return result;
 }
 
@@ -214,6 +266,9 @@ private:
 
 	std::mt19937 m_generator;
 	std::uniform_int_distribution< std::uint16_t > m_ports_generator;
+	std::uniform_int_distribution< std::int32_t > m_connect_delay_generator;
+	std::uniform_int_distribution< std::int32_t > m_send_delay_generator;
+	std::uniform_int_distribution< std::int32_t > m_receive_delay_generator;
 
 	unsigned int m_active_requests{};
 	unsigned long m_initiated_requests{};
@@ -242,7 +297,10 @@ public:
 	request_performer_t(
 		manager_t * manager,
 		asio::ip::tcp::endpoint proxy_addr,
-		asio::ip::tcp::endpoint target_addr );
+		asio::ip::tcp::endpoint target_addr,
+		std::chrono::milliseconds connect_delay,
+		std::chrono::milliseconds send_delay,
+		std::chrono::milliseconds receive_delay );
 
 	void
 	start();
@@ -253,8 +311,18 @@ private:
 	const asio::ip::tcp::endpoint m_proxy_addr;
 	const asio::ip::tcp::endpoint m_target_addr;
 
+	const std::chrono::milliseconds m_connect_delay;
+	const std::chrono::milliseconds m_send_delay;
+	const std::chrono::milliseconds m_receive_delay;
+
 	void
 	run();
+
+	void
+	initiate_connect_attempt();
+
+	void
+	do_connect_attempt();
 
 	void
 	complete( completion_t completion );
@@ -264,9 +332,21 @@ private:
 		const asio::error_code & ec );
 
 	void
+	initiate_send_attempt();
+
+	void
+	do_send_attempt();
+
+	void
 	on_write_result(
 		const asio::error_code & ec,
 		std::size_t bytes_transferred );
+
+	void
+	initiate_receive_attempt();
+
+	void
+	do_receive_attempt();
 
 	void
 	on_read_result(
@@ -277,6 +357,35 @@ private:
 //
 // manager_t implementation
 //
+namespace manager_impl
+{
+
+[[nodiscard]]
+std::int32_t
+delay_generator_right_border( std::chrono::milliseconds max_value ) noexcept
+{
+	if( is_delay_defined( max_value ) )
+		return static_cast<int32_t>(max_value.count());
+	else
+		return 1;
+}
+
+template< typename Random_Generator, typename Distribution >
+[[nodiscard]]
+std::chrono::milliseconds
+generate_delay(
+	std::chrono::milliseconds max_delay,
+	Random_Generator & generator,
+	Distribution & distribution )
+{
+	if( is_delay_defined( max_delay ) )
+		return std::chrono::milliseconds{ distribution( generator ) };
+	else
+		return no_delay_marker();
+}
+
+} /* manager_impl */
+
 manager_t::manager_t(
 	asio::io_context & io_ctx,
 	cmd_line_args_t config )
@@ -286,6 +395,21 @@ manager_t::manager_t(
 	,	m_ports_generator{
 			m_config.m_port_range_left,
 			m_config.m_port_range_right
+		}
+	,	m_connect_delay_generator{
+			0,
+			manager_impl::delay_generator_right_border(
+					m_config.m_max_connect_delay )
+		}
+	,	m_send_delay_generator{
+			0,
+			manager_impl::delay_generator_right_border(
+					m_config.m_max_send_delay )
+		}
+	,	m_receive_delay_generator{
+			0,
+			manager_impl::delay_generator_right_border(
+					m_config.m_max_receive_delay )
 		}
 {}
 
@@ -352,7 +476,19 @@ manager_t::launch_new_request()
 			asio::ip::tcp::endpoint{ m_config.m_proxy_addr, port },
 			asio::ip::tcp::endpoint{
 					m_config.m_target_addr,
-					m_config.m_target_port }
+					m_config.m_target_port },
+			manager_impl::generate_delay(
+					m_config.m_max_connect_delay,
+					m_generator,
+					m_connect_delay_generator ),
+			manager_impl::generate_delay(
+					m_config.m_max_send_delay,
+					m_generator,
+					m_send_delay_generator ),
+			manager_impl::generate_delay(
+					m_config.m_max_receive_delay,
+					m_generator,
+					m_receive_delay_generator )
 		);
 	new_worker->start();
 
@@ -366,11 +502,17 @@ manager_t::launch_new_request()
 request_performer_t::request_performer_t(
 	manager_t * manager,
 	asio::ip::tcp::endpoint proxy_addr,
-	asio::ip::tcp::endpoint target_addr )
+	asio::ip::tcp::endpoint target_addr,
+	std::chrono::milliseconds connect_delay,
+	std::chrono::milliseconds send_delay,
+	std::chrono::milliseconds receive_delay )
 	:	m_manager{ manager }
 	,	m_connection{ manager->io_context() }
 	,	m_proxy_addr{ proxy_addr }
 	,	m_target_addr{ target_addr }
+	,	m_connect_delay{ connect_delay }
+	,	m_send_delay{ send_delay }
+	,	m_receive_delay{ receive_delay }
 {
 }
 
@@ -384,6 +526,33 @@ request_performer_t::start()
 
 void
 request_performer_t::run()
+{
+	initiate_connect_attempt();
+}
+
+void
+request_performer_t::initiate_connect_attempt()
+{
+	if( !is_delay_defined( m_connect_delay ) )
+		do_connect_attempt();
+	else
+	{
+		auto timer = std::make_shared< asio::steady_timer >(
+				m_connection.get_executor() );
+		timer->expires_after( m_connect_delay );
+		timer->async_wait(
+				[self = shared_from_this(), timer]
+				( const auto & ec ) {
+					if( !ec )
+						self->do_connect_attempt();
+					else
+						self->complete( completion_t::failure );
+				} );
+	}
+}
+
+void
+request_performer_t::do_connect_attempt()
 {
 	try
 	{
@@ -424,38 +593,65 @@ request_performer_t::on_connect_result(
 	}
 	else
 	{
-		try
-		{
-			auto request = std::make_shared< std::string >();
-			fmt::format_to( std::back_inserter(*request),
-					"GET http://{}/?first-param=first-value&"
-						"second-param=second-value&last-param=last-value HTTP/1.1\r\n"
-					"Host: {}\r\n"
-					"Connection: close\r\n"
-					"Content-Type: text/plain\r\n"
-					"Accept: text/plain\r\n"
-					"Content-Length: 0\r\n"
-					"\r\n",
-					m_target_addr,
-					m_target_addr );
+		initiate_send_attempt();
+	}
+}
 
-			asio::async_write(
-					m_connection,
-					asio::buffer( *request ),
-					[self = shared_from_this(), req = request](
-						const asio::error_code & ec,
-						std::size_t bytes_transferred )
-					{
-						self->on_write_result( ec, bytes_transferred );
-					} );
-		}
-		catch( const std::exception & x )
-		{
-			fmt::print( std::cerr,
-					"exception in request_performer_t::on_connect_result: {}\n",
-					x.what() );
-			complete( completion_t::failure );
-		}
+void
+request_performer_t::initiate_send_attempt()
+{
+	if( !is_delay_defined( m_send_delay ) )
+		do_send_attempt();
+	else
+	{
+		auto timer = std::make_shared< asio::steady_timer >(
+				m_connection.get_executor() );
+		timer->expires_after( m_send_delay );
+		timer->async_wait(
+				[self = shared_from_this(), timer]
+				( const auto & ec ) {
+					if( !ec )
+						self->do_send_attempt();
+					else
+						self->complete( completion_t::failure );
+				} );
+	}
+}
+
+void
+request_performer_t::do_send_attempt()
+{
+	try
+	{
+		auto request = std::make_shared< std::string >();
+		fmt::format_to( std::back_inserter(*request),
+				"GET http://{}/?first-param=first-value&"
+					"second-param=second-value&last-param=last-value HTTP/1.1\r\n"
+				"Host: {}\r\n"
+				"Connection: close\r\n"
+				"Content-Type: text/plain\r\n"
+				"Accept: text/plain\r\n"
+				"Content-Length: 0\r\n"
+				"\r\n",
+				m_target_addr,
+				m_target_addr );
+
+		asio::async_write(
+				m_connection,
+				asio::buffer( *request ),
+				[self = shared_from_this(), req = request](
+					const asio::error_code & ec,
+					std::size_t bytes_transferred )
+				{
+					self->on_write_result( ec, bytes_transferred );
+				} );
+	}
+	catch( const std::exception & x )
+	{
+		fmt::print( std::cerr,
+				"exception in request_performer_t::on_connect_result: {}\n",
+				x.what() );
+		complete( completion_t::failure );
 	}
 }
 
@@ -473,27 +669,54 @@ request_performer_t::on_write_result(
 	}
 	else
 	{
-		try
-		{
-			auto response = std::make_shared< std::string >();
-			asio::async_read_until(
-					m_connection,
-					asio::dynamic_buffer( *response ),
-					"'last-param' => 'last-value'",
-					[self = shared_from_this(), resp = response](
-						const asio::error_code & ec,
-						std::size_t bytes_transferred )
-					{
-						self->on_read_result( ec, bytes_transferred );
-					} );
-		}
-		catch( const std::exception & x )
-		{
-			fmt::print( std::cerr,
-					"exception in request_performer_t::on_read_result: {}\n",
-					x.what() );
-			complete( completion_t::failure );
-		}
+		initiate_receive_attempt();
+	}
+}
+
+void
+request_performer_t::initiate_receive_attempt()
+{
+	if( !is_delay_defined( m_receive_delay ) )
+		do_receive_attempt();
+	else
+	{
+		auto timer = std::make_shared< asio::steady_timer >(
+				m_connection.get_executor() );
+		timer->expires_after( m_receive_delay );
+		timer->async_wait(
+				[self = shared_from_this(), timer]
+				( const auto & ec ) {
+					if( !ec )
+						self->do_receive_attempt();
+					else
+						self->complete( completion_t::failure );
+				} );
+	}
+}
+
+void
+request_performer_t::do_receive_attempt()
+{
+	try
+	{
+		auto response = std::make_shared< std::string >();
+		asio::async_read_until(
+				m_connection,
+				asio::dynamic_buffer( *response ),
+				"'last-param' => 'last-value'",
+				[self = shared_from_this(), resp = response](
+					const asio::error_code & ec,
+					std::size_t bytes_transferred )
+				{
+					self->on_read_result( ec, bytes_transferred );
+				} );
+	}
+	catch( const std::exception & x )
+	{
+		fmt::print( std::cerr,
+				"exception in request_performer_t::on_read_result: {}\n",
+				x.what() );
+		complete( completion_t::failure );
 	}
 }
 
